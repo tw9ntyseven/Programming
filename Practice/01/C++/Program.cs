d89182e66d637c6d60ae7d01c799959ac006205b
tw9ntyseven <vladakmilicin@mail.ru>
2020-09-18 23:50:44 +0300

    add 01

---
+++ Practice/01/C++/Program.cs
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <locale>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a;
+    a = 2 + 2 * 2;
+    std::cout << "Результат выражения 2+2*2 = " << a << endl;
+ }
