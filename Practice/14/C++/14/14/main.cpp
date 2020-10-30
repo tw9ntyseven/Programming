13aeb85f76f233da23555c0c66b7f919320857d1
tw9ntyseven <vladakmilicin@mail.ru>
2020-10-30 19:25:33 +0300

    add 14 c++, 15 Practice

--- Practice/14/C++/14/14/main.cpp
+++ Practice/14/C++/14/14/main.cpp
@@ -1,51 +1,24 @@
-//
-//  main.cpp
-//  14
-//
-//  Created by Admin on 30.10.2020.
-//
-
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
+
+int Elevate(int a) {
+	if (a == 0)
+		return 0;
+
+	int var = 2;
+	int count = 1;
+	while (var < a)
+	{
+		var *= 2;
+		count++;
+	}
+	return count;
+}
 
 int main() {
-    std::cout << "---------------------Поробуй угадать число-------------------------\n";
+	int num;
+	std::cin >> num;
+	std::cout << Elevate(num) << '\n';
 
-A:
-    // Random time
-    int n = rand() % 100 + 1;
-
-    int a;
-    bool res = true;
-    int t = -4;
-
-    for (int i = 0; i < 5; i++) {
-        std::cin >> a;
-
-        if (a < n && t) {
-            std::cout << "Загаданное число больше\n";
-        }
-        if (a > n && t) {
-            std::cout << "Загаданное число меньше\n";
-        }
-        if (a == n && t) {
-            std::cout << "Поздравляю! Вы угадали\n";
-            res = false;
-            break;
-        }
-        t++;
-    }
-    if (res) {
-        std::cout << "Вы проиграли. Было загадано:" << n << '\n';
-    }
-
-    int c = 0;
-    std::cout << "Хотите начать сначала? (1 - ДА )";
-    std::cin >> c;
-
-    if (c == 1) {
-        goto A;
-    }
-    return 0;
+	system("pause");
+	return 0;
 }
---
+++ Practice/15/C++/15/15/main.cpp
@@ -0,0 +1,51 @@
+//
+//  main.cpp
+//  15
+//
+//  Created by Admin on 30.10.2020.
+//
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+int main() {
+    std::cout << "---------------------Поробуй угадать число-------------------------\n";
+
+A:
+    // Random time
+    int n = rand() % 100 + 1;
+
+    int a;
+    bool res = true;
+    int t = -4;
+
+    for (int i = 0; i < 5; i++) {
+        std::cin >> a;
+
+        if (a < n && t) {
+            std::cout << "Загаданное число больше\n";
+        }
+        if (a > n && t) {
+            std::cout << "Загаданное число меньше\n";
+        }
+        if (a == n && t) {
+            std::cout << "Поздравляю! Вы угадали\n";
+            res = false;
+            break;
+        }
+        t++;
+    }
+    if (res) {
+        std::cout << "Вы проиграли. Было загадано:" << n << '\n';
+    }
+
+    int c = 0;
+    std::cout << "Хотите начать сначала? (1 - ДА )";
+    std::cin >> c;
+
+    if (c == 1) {
+        goto A;
+    }
+    return 0;
+}
