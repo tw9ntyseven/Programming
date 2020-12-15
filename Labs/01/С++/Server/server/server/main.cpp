ea1259f55b0670a5d49cbb86521d717e57821390
tw9ntyseven <vladakmilicin@mail.ru>
2020-12-15 23:35:55 +0300

    rewrite lab 01

--- Labs/01/С++/Server/server/server/main.cpp
+++ Labs/01/С++/Server/server/server/main.cpp
@@ -9,11 +9,10 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
-#include <chrono>
+#include <chrono> // для функций из std::chrono
 #include <fstream>
 #include "../include/cpp_httplib/httplib.h"
 #include "../include/nlohmann/json.hpp"
-
 
 using json = nlohmann::json;
 using namespace httplib;
@@ -21,15 +20,15 @@ using namespace httplib;
 std::string html_str;
 
 
-void json_init(const Result& res, json& new_json)
-{
+void json_init(const Result& res, json& new_json) {
     if (res) {
-        if (res->status == 200) {
+        if (res->status == 200)
             new_json = json::parse(res->body);
-        } else {
+        else
             std::cout << "Status code: " << res->status << std::endl;
-        }
-    } else {
+    }
+    else
+    {
         auto err = res.error();
         std::cout << "Error code: " << err << std::endl;
     }
@@ -46,11 +45,11 @@ std::string current_time_str() {
     if (date_new[11] != '0')
         time_now += date_new[11];
     time_now += date_new[12];
-    
     return time_now;
 }
 
-int current_time_int() {
+int current_time_int()
+{
     auto current_time = std::chrono::system_clock::now();
 
     std::time_t current_time_t = std::chrono::system_clock::to_time_t(current_time);
@@ -65,34 +64,34 @@ int current_time_int() {
     return atoi(time_now.c_str());
 }
 
-void html_editing(std::string& html_str, const std::string& raw_arg, const std::string& arg) {
+void html_editing(std::string& html_str, const std::string& raw_arg, const std::string& arg)
+{
     size_t position = html_str.find(raw_arg);
-    while (position != std::string::npos) {
+    while (position != std::string::npos)
+    {
         html_str.replace(position, raw_arg.size(), arg);
         position = html_str.find(raw_arg, position + arg.size());
     }
 }
 
-void gen_response(const Request& req, Response& res) {
+void gen_response(const Request& req, Response& res)
+{
     html_editing(html_str, "{hourly[i].temp}", current_time_str());
 
     json tmp;
     std::fstream cache("cache.txt");
 
-    if (!cache.is_open()) {
+    if (!cache.is_open())
         std::cerr << "Error!\nFile not open\n";
-    } else {
+    else
         cache >> tmp;
-    }
 
     std::string description_raw = tmp[current_time_int()]["description"].dump();
     
     std::string description;
-    for (int i = 0; i < description_raw.size(); i++) {
-        if (description_raw[i] != char(34) && description_raw[i] != '\\') {
+    for (int i = 0; i < description_raw.size(); i++)
+        if (description_raw[i] != char(34) && description_raw[i] != '\\')
             description += description_raw[i];
-        }
-    }
 
     html_editing(html_str, "{hourly[i].weather[0].description}", description);
 
@@ -108,42 +107,44 @@ void gen_response(const Request& req, Response& res) {
     res.set_content(html_str, "text/html;charset=utf-8");
 }
 
-void gen_response_raw(const Request& req, Response& res) {
+void gen_response_raw(const Request& req, Response& res)
+{
     std::fstream cache("cache.txt");
     std::string rawR;
-    if (cache.is_open()) {
+    if (cache.is_open())
         getline(cache, rawR, '\0');
-    } else {
+    else
+    {
         std::cerr << "Error!\nFile not open\n";
         return;
     }
     std::string raw;
-    for (int i = 0; i < rawR.length(); i++) {
-        if (rawR[i] == '\\') {
+    for (int i = 0; i < rawR.length(); i++)
+    {
+        if (rawR[i] == '\\')
+        {
             i++;
             continue;
         }
         raw += rawR[i];
     }
+
     res.set_content(raw, "text/plain;charset=utf-8");
 }
 
 int main() {
     Server svr;
     
-    // User Client get data
-    Client openweather_cli("http://api.openweathermap.org");
-    auto openweather_res = openweather_cli.Get("/data/2.5/onecall?id=eff249724eb9589a6c6a62e8dfb5578f&lang=ru&units=metric&lat=44.95719&lon=34.11079&exclude=current,minutely,daily,alerts");
 
-    // get json weather
+    Client openweather_cli("http://api.openweathermap.org");
+    auto openweather_res = openweather_cli.Get("/data/2.5/onecall?id=524901&appid=eff249724eb9589a6c6a62e8dfb5578f&lang=ru&units=metric&lat=44.95719&lon=34.11079&exclude=current,minutely,daily,alerts");
+
     json openweather_json;
     json_init(openweather_res, openweather_json);
  
-    // take worldtime with api
     Client worldtime_cli("http://worldtimeapi.org");
     auto worldtime_res = worldtime_cli.Get("/api/timezone/Europe/Simferopol");
     
-    // get json time
     json worldtime_json;
     json_init(worldtime_res, worldtime_json);
   
@@ -151,7 +152,8 @@ int main() {
 
     json* tmp_arr = new json[48];
 
-    for (int i = 0; i < 48; i++) {
+    for (int i = 0; i < 48; i++)
+    {
         tmp_arr[i]["time"] = i;
         tmp_arr[i]["temp"] = (int)openweather_json["hourly"][i]["temp"];
         std::string  description  = openweather_json["hourly"][i]["weather"][0]["description"].dump();
@@ -166,20 +168,18 @@ int main() {
     std::string cache_str = tmp.dump();
     std::ofstream cache("cache.txt");
 
-    if (!cache.is_open()) {
+    if (!cache.is_open())
         std::cerr << "Error!\nFile not open\n";
-    } else {
+    else
         cache << cache_str;
-    }
     cache.close();
 
     std::ifstream html_file("widget.html");
 
-    if (html_file.is_open()) {
+    if (html_file.is_open())
         getline(html_file, html_str, '\0');
-    } else {
+    else
         std::cerr << "Error!\nFile not open\n";
-    }
 
     html_file.close();
 
